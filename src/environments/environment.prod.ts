328cc66cd893704796dafea8880d5fe80492d6a5
Mr3O5 <46283095+Mr3O5@users.noreply.github.com>
2021-08-16 17:06:10 +0200

    bugfixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    
+    apiKey: "AIzaSyA-_jVYMBHRJFUO0eDHBmBWK19vGMNuivQ",
+    authDomain: "ring-of-fire-b68f7.firebaseapp.com",
+    projectId: "ring-of-fire-b68f7",
+    storageBucket: "ring-of-fire-b68f7.appspot.com",
+    messagingSenderId: "278607994417",
+    appId: "1:278607994417:web:aefa55165387ba055dff10"
+  
+  }
 };
